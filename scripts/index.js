a5edf3b8051d63d73c30f64de3003b412e9fddb2
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-11-17 22:19:31 +0530

    Update index.js
    Added functionality to display thank you message when a valid email is entered. And return to landing page when dismiss thank you message is clicked.

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
 let subscribeButton = document.getElementById("subscribe_button");
 let email = document.getElementById("email_address");
+let signup_form = document.getElementById("landing_page");
+let success_message = document.getElementById("thanks_message");
+let dismissButton = document.getElementById("dismiss_button");
 
 function subscribed() {
   if (email.value.includes("@")) {
-    alert("Subscribed Succesfully");
+    signup_form.classList.add("hidden");
+    success_message.classList.remove("hidden");
   } else {
     alert("Please enter a valid email");
   }
+  console.log(signup_form.classList);
+}
+
+function reset() {
+  signup_form.classList.remove("hidden");
+  success_message.classList.add("hidden");
 }
 
 subscribeButton.addEventListener("click", subscribed);
+dismissButton.addEventListener("click", reset);
