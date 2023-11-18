04982f308aea266960e833bfb051c121fd89c804
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-11-18 22:29:58 +0530

    Update index.js
    Added logic for unsuccesful email attempts.
    
    Error states are visible by removing and adding relevant "hidden" and "error state" classes to the error_label and email elements respectively.

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,16 @@ let email = document.getElementById("email_address");
 let signup_form = document.getElementById("landing_page");
 let success_message = document.getElementById("thanks_message");
 let dismissButton = document.getElementById("dismiss_button");
+let error_label = document.getElementById("error_label");
 
 function subscribed() {
   if (email.value.includes("@")) {
     signup_form.classList.add("hidden");
     success_message.classList.remove("hidden");
   } else {
-    alert("Please enter a valid email");
+    error_label.classList.remove("hidden");
+    email.classList.add("error_state");
   }
-  console.log(signup_form.classList);
 }
 
 function reset() {
